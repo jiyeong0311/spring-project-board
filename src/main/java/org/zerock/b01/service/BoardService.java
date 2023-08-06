d7ee9982d4c91a910e1fb472a9ce57a79e5909cc
jiyeong0311 <peach031120@gmail.com>
2023-08-06 22:38:21 +0900

    2023.08.06

--- src/main/java/org/zerock/b01/service/BoardService.java
+++ src/main/java/org/zerock/b01/service/BoardService.java
@@ -1,2 +1,22 @@
-package org.zerock.b01.service;public interface BoardService {
+package org.zerock.b01.service;
+
+import org.zerock.b01.dto.BoardDTO;
+import org.zerock.b01.dto.PageRequestDTO;
+import org.zerock.b01.dto.PageResponseDTO;
+
+public interface BoardService {
+
+    // 등록 작업 처리
+    Long register(BoardDTO boardDTO);
+
+    // 조회 작업 처리
+    BoardDTO readOne(Long bno);
+
+    // 수정 작업 처리
+    void modify(BoardDTO boardDTO);
+
+    // 삭제 작업 처리
+    void remove(Long bno);
+
+    PageResponseDTO<BoardDTO> list(PageRequestDTO pageRequestDTO);
 }
